b1e8db26d77d6fa0f6aec05f5cbc26333e5e86fc
Thúy Vương <nguyenthithuyvuong11112005@gmail.com>
2026-08-30 03:07:22 +0700

    Tạo giao diện Trang chủ

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-          Giao dien 
-      </h1>
-    </div>
-  )
+  return <HomePage />;
 }
 
-export default App
+export default App;
